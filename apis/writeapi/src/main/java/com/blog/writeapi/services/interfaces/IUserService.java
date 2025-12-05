83f727c89f606f44e5d36b3da7999bfe7e4f1e73
andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-12-05 16:45:00 -0300

    Impled UserMapper with your maps

---
+++ apis/writeapi/src/main/java/com/blog/writeapi/services/interfaces/IUserService.java
@@ -0,0 +1,13 @@
+package com.blog.writeapi.services.interfaces;
+
+import com.blog.writeapi.dtos.user.UpdateUserDTO;
+import com.blog.writeapi.models.UserModel;
+
+import java.util.Optional;
+
+public interface IUserService {
+    Optional<UserModel> GetById(Long id);
+    Boolean ExistsById(Long id);
+    void Delete(UserModel user);
+    UserModel Update(UpdateUserDTO dto, UserModel user);
+}
---
+++ apis/writeapi/src/main/java/com/blog/writeapi/utils/mappers/UserMapper.java
@@ -0,0 +1,27 @@
+package com.blog.writeapi.utils.mappers;
+
+import com.blog.writeapi.dtos.user.UpdateUserDTO;
+import com.blog.writeapi.dtos.user.UserDTO;
+import com.blog.writeapi.models.UserModel;
+import com.github.dozermapper.core.Mapper;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserMapper {
+    private final Mapper mapper;
+
+    public UserModel toModel(UserDTO dto) {
+        return mapper.map(dto, UserModel.class);
+    }
+
+    public UserDTO toDTO(UserModel user) {
+        return mapper.map(user, UserDTO.class);
+    }
+
+    public void merge(UpdateUserDTO dto, UserModel target) {
+        mapper.map(dto, target);
+    }
+
+}
