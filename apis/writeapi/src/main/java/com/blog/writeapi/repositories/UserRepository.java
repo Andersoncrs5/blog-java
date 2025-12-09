9df6532c36736851c06e71eaf739bd7a118c9fbf
andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-12-09 20:33:59 -0300

    Little Improvements

--- apis/writeapi/src/main/java/com/blog/writeapi/repositories/UserRepository.java
+++ apis/writeapi/src/main/java/com/blog/writeapi/repositories/UserRepository.java
@@ -1,9 +1,10 @@
 package com.blog.writeapi.repositories;
 
 import com.blog.writeapi.models.UserModel;
+import org.jspecify.annotations.NonNull;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserRepository extends JpaRepository<UserModel, Long> {
+public interface UserRepository extends JpaRepository<@NonNull UserModel, @NonNull Long> {
 }
