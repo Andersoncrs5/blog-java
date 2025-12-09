01984de2ae737c9987ce9b62b44568d49f02fe41
andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-12-09 20:29:20 -0300

    New table role

---
+++ apis/writeapi/src/main/java/com/blog/writeapi/models/RoleModel.java
@@ -0,0 +1,35 @@
+package com.blog.writeapi.models;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "roles", indexes = {
+        @Index(name = "idx_name", columnList = "name")
+})
+@EntityListeners(AuditingEntityListener.class)
+@Setter
+@Getter
+public class RoleModel {
+    @Id
+    @Column(name = "id", columnDefinition = "BIGINT UNSIGNED")
+    private Long id;
+
+    @Column(length = 100, nullable = false, unique = true, updatable = false)
+    private String name;
+
+    @Column
+    private Boolean isActive = true;
+
+    @CreatedDate
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
+}
