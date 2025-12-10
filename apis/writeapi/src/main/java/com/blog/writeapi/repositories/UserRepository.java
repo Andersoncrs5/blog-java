6c9a71fe5731c68e7855bf4837fe5aa78d4ba934
andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-12-10 09:59:16 -0300

    New method findByEmail

--- apis/writeapi/src/main/java/com/blog/writeapi/repositories/UserRepository.java
+++ apis/writeapi/src/main/java/com/blog/writeapi/repositories/UserRepository.java
@@ -3,8 +3,10 @@ package com.blog.writeapi.repositories;
 import com.blog.writeapi.models.UserModel;
 import org.jspecify.annotations.NonNull;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends JpaRepository<@NonNull UserModel, @NonNull Long> {
+    UserDetails findByEmail(String email);
 }
