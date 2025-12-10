c256eb7db9d3c51e038ebfc98c20e0d7b05453cf
andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-12-09 21:01:20 -0300

    New table UserRoleModel

---
+++ apis/writeapi/src/main/java/com/blog/writeapi/models/UserRoleModel.java
@@ -0,0 +1,37 @@
+package com.blog.writeapi.models;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "user_roles", indexes = {
+        @Index(name = "idx_user_id", columnList = "user_id"),
+        @Index(name = "idx_role_id", columnList = "role_id"),
+        @Index(name = "uk_user_role", columnList = "user_id, role_id", unique = true)
+})
+@EntityListeners(AuditingEntityListener.class)
+@Setter
+@Getter
+public class UserRoleModel {
+
+    @Id
+    @Column(name = "id", columnDefinition = "BIGINT UNSIGNED")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "user_id", nullable = false)
+    private UserModel user;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "role_id", nullable = false)
+    private RoleModel role;
+
+    @CreatedDate
+    private LocalDateTime createdAt;
+
+}
