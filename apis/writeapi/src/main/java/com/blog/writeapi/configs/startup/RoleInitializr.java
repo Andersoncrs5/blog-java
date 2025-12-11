e1af43d0c8dc1b50ab468e91794d77661cb4764f
andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-12-11 19:55:39 -0300

    Set up RoleInitializr

---
+++ apis/writeapi/src/main/java/com/blog/writeapi/configs/startup/RoleInitializr.java
@@ -0,0 +1,42 @@
+package com.blog.writeapi.configs.startup;
+
+import com.blog.writeapi.models.RoleModel;
+import com.blog.writeapi.services.providers.RoleService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class RoleInitializr implements CommandLineRunner {
+
+    private final RoleService service;
+
+    @Override
+    public void run(String... args) throws Exception {
+        createRole("USER_ROLE");
+        createRole("ADM_ROLE");
+        createRole("SUPER_ADM_ROLE");
+        createRole("MODERATOR_ROLE");
+    }
+
+    void createRole(String name) {
+        Boolean exists = this.service.existsByName(name);
+
+        if (exists) {
+            log.debug("Role with name {} already exists", name);
+            return;
+        }
+
+        RoleModel role = new RoleModel().toBuilder()
+                .name(name.toUpperCase())
+                .build();
+
+        RoleModel model = this.service.create(role);
+        log.debug("Role {} created!", model.getName());
+
+    }
+
+}
