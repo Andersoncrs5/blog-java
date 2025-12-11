0c6530d52de8a3f888022a49c9fd4cf02f6863e4
andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-12-11 20:53:19 -0300

    Fix url auth/login and auth/register was putted v1/ in front it

--- apis/writeapi/src/main/java/com/blog/writeapi/configs/security/SecurityConfig.java
+++ apis/writeapi/src/main/java/com/blog/writeapi/configs/security/SecurityConfig.java
@@ -48,8 +48,8 @@ public class SecurityConfig {
                 .csrf(AbstractHttpConfigurer::disable)
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(authorize -> authorize
-                        .requestMatchers(HttpMethod.POST, "/auth/login").permitAll()
-                        .requestMatchers(HttpMethod.POST, "/auth/register").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/v1/auth/login").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/v1/auth/register").permitAll()
 
                         .requestMatchers(HttpMethod.GET, "/v3/api-docs/**").permitAll()
                         .requestMatchers(HttpMethod.GET, "/swagger-ui/**").permitAll()
