fd447444b58d2ffc16bb51da465d8cc9478dad56
andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-12-12 21:41:59 -0300

    New docs to method login

--- apis/writeapi/src/main/java/com/blog/writeapi/controllers/docs/AuthControllerDocs.java
+++ apis/writeapi/src/main/java/com/blog/writeapi/controllers/docs/AuthControllerDocs.java
@@ -1,6 +1,7 @@
 package com.blog.writeapi.controllers.docs;
 
 import com.blog.writeapi.dtos.user.CreateUserDTO;
+import com.blog.writeapi.dtos.user.LoginUserDTO;
 import com.blog.writeapi.utils.res.ResponseHttp;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
@@ -11,6 +12,7 @@ import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 public interface AuthControllerDocs {
 
@@ -23,4 +25,9 @@ public interface AuthControllerDocs {
                     schema = @Schema(implementation = ResponseHttp.class)))
     @ApiResponse(responseCode = "500", description = "Error internal in server")
     ResponseEntity<?> Create(@Valid @RequestBody CreateUserDTO dto, HttpServletRequest request);
+
+    @PostMapping("/login")
+    @Operation(summary = "Login user", tags = {"Auth"})
+    @ApiResponse(responseCode = "401", description = "Login invalid")
+    ResponseEntity<?> login(@Valid @RequestBody LoginUserDTO dto, HttpServletRequest request);
 }
