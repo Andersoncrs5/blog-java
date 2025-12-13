3bf44f8fc191100b5bbead30ec37b0352b923659
andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-12-12 21:03:33 -0300

    Added annotation Autowired to UserRepository

--- apis/writeapi/src/main/java/com/blog/writeapi/utils/annotations/valid/user/uniqueEmail/UniqueEmailValidator.java
+++ apis/writeapi/src/main/java/com/blog/writeapi/utils/annotations/valid/user/uniqueEmail/UniqueEmailValidator.java
@@ -3,10 +3,12 @@ package com.blog.writeapi.utils.annotations.valid.user.uniqueEmail;
 import com.blog.writeapi.repositories.UserRepository;
 import jakarta.validation.ConstraintValidator;
 import jakarta.validation.ConstraintValidatorContext;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class UniqueEmailValidator implements ConstraintValidator<UniqueEmail, String> {
+    @Autowired
     private UserRepository repository;
 
     @Override
