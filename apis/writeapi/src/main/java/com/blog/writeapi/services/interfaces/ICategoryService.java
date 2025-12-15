3371d839c4028e083e314fb0643bd72bc9ff7efb
andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-12-14 21:21:28 -0300

    Set up CategoryService already with methods

---
+++ apis/writeapi/src/main/java/com/blog/writeapi/services/interfaces/ICategoryService.java
@@ -0,0 +1,19 @@
+package com.blog.writeapi.services.interfaces;
+
+import com.blog.writeapi.dtos.category.CreateCategoryDTO;
+import com.blog.writeapi.dtos.category.UpdateCategoryDTO;
+import com.blog.writeapi.models.CategoryModel;
+
+import java.util.Optional;
+
+public interface ICategoryService {
+    Optional<CategoryModel> getById(Long id);
+    Boolean existsById(Long id);
+    void delete(CategoryModel category);
+    CategoryModel create(CreateCategoryDTO dto);
+    Optional<CategoryModel> getByName(String name);
+    Boolean existsByName(String name);
+    Optional<CategoryModel> getBySlug(String slug);
+    Boolean existsBySlug(String slug);
+    CategoryModel update(UpdateCategoryDTO dto, CategoryModel category);
+}
---
+++ apis/writeapi/src/main/java/com/blog/writeapi/services/providers/CategoryService.java
@@ -0,0 +1,62 @@
+package com.blog.writeapi.services.providers;
+
+
+import cn.hutool.core.lang.Snowflake;
+import com.blog.writeapi.dtos.category.CreateCategoryDTO;
+import com.blog.writeapi.dtos.category.UpdateCategoryDTO;
+import com.blog.writeapi.models.CategoryModel;
+import com.blog.writeapi.repositories.CategoryRepository;
+import com.blog.writeapi.services.interfaces.ICategoryService;
+import com.blog.writeapi.utils.mappers.CategoryMapper;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class CategoryService implements ICategoryService {
+
+    private final CategoryRepository repository;
+    private final Snowflake generator;
+    private final CategoryMapper mapper;
+
+    @Override
+    public Optional<CategoryModel> getById(Long id) { return this.repository.findById(id); }
+
+    @Override
+    public Boolean existsById(Long id) { return repository.existsById(id); }
+
+    @Override
+    public void delete(CategoryModel category) { this.repository.delete(category); }
+
+    @Override
+    public Optional<CategoryModel> getByName(String name) { return this.repository.findByName(name); }
+
+    @Override
+    public Boolean existsByName(String name) { return repository.existsByName(name); }
+
+    @Override
+    public Optional<CategoryModel> getBySlug(String slug) { return this.repository.findBySlug(slug); }
+
+    @Override
+    public Boolean existsBySlug(String slug) { return repository.existsBySlug(slug); }
+
+    @Override
+    public CategoryModel create(CreateCategoryDTO dto) {
+        CategoryModel category = this.mapper.toModel(dto);
+        category.setId(generator.nextId());
+
+        return this.repository.save(category);
+    }
+
+    @Override
+    public CategoryModel update(UpdateCategoryDTO dto, CategoryModel category) {
+        mapper.merge(dto, category);
+
+        return this.repository.save(category);
+    }
+
+}
