d20f1de1973c9628e620dee3096a1e4d472a26e2
andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-12-14 21:20:28 -0300

    Impled CategoryDTO

---
+++ apis/writeapi/src/main/java/com/blog/writeapi/dtos/category/CategoryDTO.java
@@ -0,0 +1,16 @@
+package com.blog.writeapi.dtos.category;
+
+import java.time.LocalDateTime;
+
+public record CategoryDTO(
+        Long id,
+        String name,
+        String description,
+        String slug,
+        Boolean isActive,
+        Boolean visible,
+        Integer displayOrder,
+        LocalDateTime createdAt,
+        LocalDateTime updatedAt
+) {
+}
