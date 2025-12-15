af637f4b622cbca55e2a5c35af3284c5a1559402
andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-12-14 21:21:09 -0300

    New methods

--- apis/writeapi/src/main/java/com/blog/writeapi/repositories/CategoryRepository.java
+++ apis/writeapi/src/main/java/com/blog/writeapi/repositories/CategoryRepository.java
@@ -4,5 +4,11 @@ import com.blog.writeapi.models.CategoryModel;
 import org.jspecify.annotations.NonNull;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface CategoryRepository extends JpaRepository<@NonNull CategoryModel, @NonNull Long> {
+    Boolean existsByName(String name);
+    Optional<CategoryModel> findByName(String name);
+    Optional<CategoryModel> findBySlug(String slug);
+    Boolean existsBySlug(String slug);
 }
