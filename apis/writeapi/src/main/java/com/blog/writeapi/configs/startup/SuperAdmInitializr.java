e11471a9a05e479d0702a84a4403911b866f0a3a
andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-12-16 20:57:45 -0300

    Create SuperAdmInitializr to init super adm when startup app

---
+++ apis/writeapi/src/main/java/com/blog/writeapi/configs/startup/SuperAdmInitializr.java
@@ -0,0 +1,69 @@
+package com.blog.writeapi.configs.startup;
+
+import com.blog.writeapi.models.RoleModel;
+import com.blog.writeapi.models.UserModel;
+import com.blog.writeapi.repositories.UserRepository;
+import com.blog.writeapi.services.providers.RoleService;
+import com.blog.writeapi.services.providers.UserRoleService;
+import io.github.resilience4j.retry.annotation.Retry;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.security.crypto.argon2.Argon2PasswordEncoder;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Random;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class SuperAdmInitializr implements CommandLineRunner {
+
+    private final UserRepository repository;
+    private final RoleService roleRepository;
+    private final UserRoleService userRoleService;
+    private final Argon2PasswordEncoder encoder;
+
+    @Override
+    @Transactional
+    @Retry(name = "super-adm-retry", fallbackMethod = "fallbackCreateCategory")
+    public void run(String... args) throws Exception {
+        String email = "system.domain@gmail.com";
+
+        UserModel superAdm = repository.findByEmail(email)
+                .orElseGet(() -> {
+                    UserModel newUser = new UserModel().toBuilder()
+                            .name("System")
+                            .username("System")
+                            .email(email)
+                            .password(this.encoder.encode("0123456789"))
+                            .build();
+                    newUser.setId(new Random().nextLong(1, Long.MAX_VALUE));
+                    return repository.save(newUser);
+                });
+
+        RoleModel role = roleRepository.findByName("SUPER_ADM_ROLE")
+                .orElseThrow(() -> new RuntimeException("Role SUPER_ADM_ROLE not found yet."));
+        applyRole(superAdm, role);
+    }
+
+    private void applyRole(UserModel user, RoleModel role) {
+        if (user == null || role == null) return;
+
+        boolean hasRole = this.userRoleService.existsByUserAndRole(user, role);
+
+        if (hasRole) {
+            log.debug("User '{}' already has the role '{}'.", user.getEmail(), role.getName());
+            return;
+        }
+
+        this.userRoleService.create(user, role);
+        log.debug("Role '{}' assigned to user '{}'.", role.getName(), user.getEmail());
+    }
+
+    public void fallbackCreateCategory(String[] args, Exception e) {
+        log.error("Todas as tentativas de criar categoria falharam: {}", e.getMessage());
+    }
+
+}
