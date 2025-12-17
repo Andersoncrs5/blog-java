35f025674ff918d3c181bbe9c3ce13643f3227e8
andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-12-17 10:14:41 -0300

    New annotation EnableJpaAuditing

--- apis/writeapi/src/main/java/com/blog/writeapi/WriteapiApplication.java
+++ apis/writeapi/src/main/java/com/blog/writeapi/WriteapiApplication.java
@@ -2,8 +2,10 @@ package com.blog.writeapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class WriteapiApplication {
 
 	public static void main(String[] args) {
