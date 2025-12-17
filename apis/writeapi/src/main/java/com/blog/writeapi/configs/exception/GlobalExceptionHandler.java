308be3db2d656085a494372161b691825589c411
andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-12-17 13:55:55 -0300

    Added GlobalExceptionHandler

---
+++ apis/writeapi/src/main/java/com/blog/writeapi/configs/exception/GlobalExceptionHandler.java
@@ -0,0 +1,39 @@
+package com.blog.writeapi.configs.exception;
+
+import com.blog.writeapi.utils.res.ResponseHttp;
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.ConstraintViolationException;
+import org.jspecify.annotations.NonNull;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.time.OffsetDateTime;
+import java.util.UUID;
+import java.util.stream.Collectors;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(ConstraintViolationException.class)
+    public ResponseEntity<@NonNull ResponseHttp<Object>> handleConstraintViolation(ConstraintViolationException ex) {
+
+        String message = ex.getConstraintViolations()
+                .stream()
+                .map(ConstraintViolation::getMessage)
+                .collect(Collectors.joining(" | "));
+
+        ResponseHttp<Object> res = new ResponseHttp<>(
+                null,
+                message,
+                UUID.randomUUID().toString(),
+                0,
+                false,
+                OffsetDateTime.now()
+        );
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(res);
+    }
+
+}
