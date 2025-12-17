c00cf88304db30064c02c083e934e0e4ae620993
andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-12-17 14:11:23 -0300

    New endpoint get and delete

--- apis/writeapi/src/main/java/com/blog/writeapi/controllers/providers/CategoryController.java
+++ apis/writeapi/src/main/java/com/blog/writeapi/controllers/providers/CategoryController.java
@@ -9,10 +9,13 @@ import com.blog.writeapi.utils.mappers.CategoryMapper;
 import com.blog.writeapi.utils.res.ResponseHttp;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
+import jakarta.validation.constraints.Positive;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -25,6 +28,7 @@ import java.util.UUID;
 @RestController
 @RequestMapping("/v1/category")
 @RequiredArgsConstructor
+@Validated
 public class CategoryController implements CategoryControllerDocs {
 
     private final CategoryService service;
@@ -68,6 +72,78 @@ public class CategoryController implements CategoryControllerDocs {
         );
 
         return ResponseEntity.status(HttpStatus.CREATED).body(res);
+    }
+
+    @Override
+    public ResponseEntity<?> get(
+            @PathVariable
+            @Positive(message = "The ID provided must be a positive number.")
+            Long id,
+            HttpServletRequest request
+    ) {
+        Optional<CategoryModel> categoryOpt = this.service.getById(id);
+
+        if (categoryOpt.isEmpty()) {
+            ResponseHttp<Object> res = new ResponseHttp<>(
+                    null,
+                    "Category not exists",
+                    UUID.randomUUID().toString(),
+                    1,
+                    false,
+                    OffsetDateTime.now()
+            );
+
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(res);
+        }
+
+        CategoryDTO categoryDto = this.mapper.toDTO(categoryOpt.get());
+
+        ResponseHttp<CategoryDTO> res = new ResponseHttp<>(
+                categoryDto,
+                "Category found with successfully",
+                UUID.randomUUID().toString(),
+                1,
+                true,
+                OffsetDateTime.now()
+        );
+
+        return ResponseEntity.status(HttpStatus.OK).body(res);
+    }
+
+    @Override
+    public ResponseEntity<?> delete(
+            @PathVariable
+            @Positive(message = "The ID provided must be a positive number.")
+            Long id,
+            HttpServletRequest request
+    ) {
+        Optional<CategoryModel> categoryOpt = this.service.getById(id);
+
+        if (categoryOpt.isEmpty()) {
+            ResponseHttp<Object> res = new ResponseHttp<>(
+                    null,
+                    "Category not exists",
+                    UUID.randomUUID().toString(),
+                    1,
+                    false,
+                    OffsetDateTime.now()
+            );
+
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(res);
+        }
+
+        this.service.delete(categoryOpt.get());
+
+        ResponseHttp<Object> res = new ResponseHttp<>(
+                null,
+                "Category deleted with successfully",
+                UUID.randomUUID().toString(),
+                1,
+                true,
+                OffsetDateTime.now()
+        );
+
+        return ResponseEntity.status(HttpStatus.OK).body(res);
     }
 
 
