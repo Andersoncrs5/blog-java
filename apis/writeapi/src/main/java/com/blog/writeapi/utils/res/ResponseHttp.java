641e004d1c205e4c583df0944a000a787eef9193
andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-12-18 16:02:38 -0300

    Added annotation to validations

--- apis/writeapi/src/main/java/com/blog/writeapi/utils/res/ResponseHttp.java
+++ apis/writeapi/src/main/java/com/blog/writeapi/utils/res/ResponseHttp.java
@@ -1,13 +1,26 @@
 package com.blog.writeapi.utils.res;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
 import java.time.OffsetDateTime;
 
 public record ResponseHttp<T>(
         T data,
+
+        @NotBlank
         String message,
+
+        @NotBlank
         String traceId,
+
+        @NotNull
         Integer version,
+
+        @NotNull
         boolean status,
+
+        @NotNull
         OffsetDateTime timestamp
 ) { }
 
