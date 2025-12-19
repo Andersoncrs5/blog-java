dbb48c3a9d2c6d7d0853d94734a6674478dfaa7d
andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-12-19 16:08:09 -0300

    New method findByIdForUpdate

--- apis/writeapi/src/main/java/com/blog/writeapi/repositories/TagRepository.java
+++ apis/writeapi/src/main/java/com/blog/writeapi/repositories/TagRepository.java
@@ -1,8 +1,14 @@
 package com.blog.writeapi.repositories;
 
 import com.blog.writeapi.models.TagModel;
+import jakarta.persistence.LockModeType;
+import jakarta.persistence.QueryHint;
 import org.jspecify.annotations.NonNull;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.jpa.repository.QueryHints;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -14,4 +20,9 @@ public interface TagRepository extends JpaRepository<@NonNull TagModel, @NonNull
 
     Optional<TagModel> findBySlug(String slug);
     Boolean existsBySlug(String slug);
+
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    @Query("SELECT t FROM TagModel t WHERE t.id = :id")
+    @QueryHints({@QueryHint(name = "jakarta.persistence.lock.timeout", value = "4000")})
+    Optional<TagModel> findByIdForUpdate(@Param("id") Long id);
 }
