728d3c8e99b5f676c36585ac9ab26ff85e503b77
andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-12-19 16:09:19 -0300

    More validations

--- apis/writeapi/src/main/java/com/blog/writeapi/dtos/tag/CreateTagDTO.java
+++ apis/writeapi/src/main/java/com/blog/writeapi/dtos/tag/CreateTagDTO.java
@@ -2,6 +2,8 @@ package com.blog.writeapi.dtos.tag;
 
 import com.blog.writeapi.utils.annotations.valid.global.StringClear.StringClear;
 import com.blog.writeapi.utils.annotations.valid.global.slug.Slug;
+import com.blog.writeapi.utils.annotations.valid.tag.existsTagByName.ExistsTagByName;
+import com.blog.writeapi.utils.annotations.valid.tag.existsTagBySlug.ExistsTagBySlug;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
@@ -11,10 +13,12 @@ public record CreateTagDTO(
         @NotBlank
         @StringClear
         @Size(min = 2, max = 70)
+        @ExistsTagByName
         String name,
 
         @Slug
         @NotBlank
+        @ExistsTagBySlug
         @Size(min = 2, max = 80)
         String slug,
 
