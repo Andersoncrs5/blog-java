3f8b34ef2440b4cd300ba3787288930758d8a784
andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-12-19 21:25:02 -0300

    Applying TagController

---
+++ apis/writeapi/src/main/java/com/blog/writeapi/controllers/providers/TagController.java
@@ -0,0 +1,57 @@
+package com.blog.writeapi.controllers.providers;
+
+import com.blog.writeapi.controllers.docs.TagControllerDocs;
+import com.blog.writeapi.dtos.tag.CreateTagDTO;
+import com.blog.writeapi.dtos.tag.TagDTO;
+import com.blog.writeapi.models.TagModel;
+import com.blog.writeapi.services.interfaces.ITagService;
+import com.blog.writeapi.utils.mappers.TagMapper;
+import com.blog.writeapi.utils.res.ResponseHttp;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.OffsetDateTime;
+import java.util.UUID;
+
+@Slf4j
+@RestController
+@RequestMapping("/v1/tag")
+@RequiredArgsConstructor
+@Validated
+public class TagController implements TagControllerDocs {
+
+    private final ITagService service;
+    private final TagMapper mapper;
+
+    @Override
+    public ResponseEntity<?> create(@Valid @RequestBody CreateTagDTO dto, HttpServletRequest request) {
+
+        TagModel tagModel = this.service.create(dto);
+
+        TagDTO tagDTO = this.mapper.toDTO(tagModel);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(new ResponseHttp<>(
+                tagDTO,
+                "Tag created with successfully",
+                UUID.randomUUID().toString(),
+                1,
+                true,
+                OffsetDateTime.now()
+        ));
+    }
+
+    private ResponseEntity<?> buildResponseError(String message, HttpStatus status) {
+        return ResponseEntity.status(status).body(new ResponseHttp<>(
+                null, message, UUID.randomUUID().toString(), 0, false, OffsetDateTime.now()
+        ));
+    }
+
+}
