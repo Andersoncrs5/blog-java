de3aa215a8959d68be293b409e361da5c9d143ce
andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-12-20 14:51:43 -0300

    Improvements in organization

--- apis/writeapi/src/main/java/com/blog/writeapi/utils/res/swagger/tag/ResponseTagDTO.java
+++ apis/writeapi/src/main/java/com/blog/writeapi/utils/res/swagger/tag/ResponseTagDTO.java
@@ -4,6 +4,6 @@ import com.blog.writeapi.dtos.tag.TagDTO;
 import com.blog.writeapi.utils.res.ResponseHttp;
 
 public record ResponseTagDTO(
-        ResponseHttp<TagDTO> dto
+        ResponseHttp<TagDTO> res
 ) {
 }
