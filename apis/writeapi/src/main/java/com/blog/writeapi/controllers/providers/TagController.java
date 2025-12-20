ea5c5bd7753f5b56d5bfd60fc7aa9e5af01efa26
andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-12-20 10:19:32 -0300

    New endpoint get

--- apis/writeapi/src/main/java/com/blog/writeapi/controllers/providers/TagController.java
+++ apis/writeapi/src/main/java/com/blog/writeapi/controllers/providers/TagController.java
@@ -9,6 +9,7 @@ import com.blog.writeapi.utils.mappers.TagMapper;
 import com.blog.writeapi.utils.res.ResponseHttp;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
+import jakarta.validation.constraints.Positive;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
@@ -19,6 +20,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.OffsetDateTime;
+import java.util.Optional;
 import java.util.UUID;
 
 @Slf4j
@@ -33,7 +35,6 @@ public class TagController implements TagControllerDocs {
 
     @Override
     public ResponseEntity<?> create(@Valid @RequestBody CreateTagDTO dto, HttpServletRequest request) {
-
         TagModel tagModel = this.service.create(dto);
 
         TagDTO tagDTO = this.mapper.toDTO(tagModel);
@@ -41,6 +42,23 @@ public class TagController implements TagControllerDocs {
         return ResponseEntity.status(HttpStatus.CREATED).body(new ResponseHttp<>(
                 tagDTO,
                 "Tag created with successfully",
+                UUID.randomUUID().toString(),
+                1,
+                true,
+                OffsetDateTime.now()
+        ));
+    }
+
+    @Override
+    public ResponseEntity<?> get(@Positive Long id, HttpServletRequest request) {
+        Optional<TagModel> optional = this.service.getById(id);
+
+        if (optional.isEmpty())
+            return this.buildResponseError("Tag not found", HttpStatus.NOT_FOUND);
+
+        return ResponseEntity.status(HttpStatus.OK).body(new ResponseHttp<>(
+                optional.get(),
+                "Tag found with successfully",
                 UUID.randomUUID().toString(),
                 1,
                 true,
