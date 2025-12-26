daa4705fac0d4689c66a39e5754d28093da8b67a
andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-12-26 14:10:20 -0300

    New methods

--- apis/writeapi/src/main/java/com/blog/writeapi/services/interfaces/IPostCategoriesService.java
+++ apis/writeapi/src/main/java/com/blog/writeapi/services/interfaces/IPostCategoriesService.java
@@ -1,4 +1,26 @@
 package com.blog.writeapi.services.interfaces;
 
+import com.blog.writeapi.dtos.postCategories.CreatePostCategoriesDTO;
+import com.blog.writeapi.dtos.postCategories.UpdatePostCategoriesDTO;
+import com.blog.writeapi.models.CategoryModel;
+import com.blog.writeapi.models.PostCategoriesModel;
+import com.blog.writeapi.models.PostModel;
+import com.blog.writeapi.utils.annotations.valid.global.isId.IsId;
+import jakarta.validation.constraints.NotNull;
+
+import java.util.Optional;
+
 public interface IPostCategoriesService {
+    Optional<PostCategoriesModel> getById(@IsId Long id);
+    PostCategoriesModel getByIdSimple(@IsId Long id);
+    void delete(@NotNull PostCategoriesModel model);
+    PostCategoriesModel create(
+            @NotNull CreatePostCategoriesDTO dto,
+            @NotNull PostModel post,
+            @NotNull CategoryModel category
+    );
+    PostCategoriesModel update(
+            @NotNull UpdatePostCategoriesDTO dto,
+            @NotNull PostCategoriesModel model
+    );
 }
--- apis/writeapi/src/main/java/com/blog/writeapi/services/providers/PostCategoriesService.java
+++ apis/writeapi/src/main/java/com/blog/writeapi/services/providers/PostCategoriesService.java
@@ -1,23 +1,75 @@
 package com.blog.writeapi.services.providers;
 
+import cn.hutool.core.lang.Snowflake;
+import com.blog.writeapi.dtos.postCategories.CreatePostCategoriesDTO;
+import com.blog.writeapi.dtos.postCategories.UpdatePostCategoriesDTO;
+import com.blog.writeapi.models.CategoryModel;
 import com.blog.writeapi.models.PostCategoriesModel;
+import com.blog.writeapi.models.PostModel;
 import com.blog.writeapi.repositories.PostCategoriesRepository;
 import com.blog.writeapi.services.interfaces.IPostCategoriesService;
 import com.blog.writeapi.utils.annotations.valid.global.isId.IsId;
+import com.blog.writeapi.utils.exceptions.ModelNotFoundException;
+import com.blog.writeapi.utils.mappers.PostCategoriesMapper;
+import io.github.resilience4j.retry.annotation.Retry;
+import jakarta.validation.constraints.NotNull;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.validation.annotation.Validated;
 
 import java.util.Optional;
 
 @Service
+@Validated
 @RequiredArgsConstructor
 public class PostCategoriesService implements IPostCategoriesService {
 
     private final PostCategoriesRepository repository;
+    private final PostCategoriesMapper mapper;
+    private final Snowflake generator;
 
+    @Override
     @Transactional(readOnly = true)
     public Optional<PostCategoriesModel> getById(@IsId Long id) {
         return this.repository.findById(id);
     }
+
+    @Transactional(readOnly = true)
+    public PostCategoriesModel getByIdSimple(@IsId Long id) {
+        return this.repository.findById(id).orElseThrow(() -> new ModelNotFoundException("Category not found"));
+    }
+
+    @Transactional
+    @Retry(name = "delete-retry")
+    public void delete(@NotNull PostCategoriesModel model) {
+        this.repository.delete(model);
+    }
+
+    @Transactional
+    @Retry(name = "create-retry")
+    public PostCategoriesModel create(
+            @NotNull CreatePostCategoriesDTO dto,
+            @NotNull PostModel post,
+            @NotNull CategoryModel category) {
+        PostCategoriesModel model = this.mapper.toModel(dto);
+
+        model.setId(generator.nextId());
+        model.setPost(post);
+        model.setCategory(category);
+
+        return this.repository.save(model);
+    }
+
+    @Transactional
+    @Retry(name = "update-retry")
+    public PostCategoriesModel update(
+            @NotNull UpdatePostCategoriesDTO dto,
+            @NotNull PostCategoriesModel model
+            ) {
+        this.mapper.merge(dto, model);
+
+        return this.repository.save(model);
+    }
+
 }
