cd3d2ae2a688f1974f28a2e0dbb611de81f36e7a
andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-12-26 14:26:29 -0300

    New method existsByPostIdAndPrimaryTrue

--- apis/writeapi/src/main/java/com/blog/writeapi/repositories/PostCategoriesRepository.java
+++ apis/writeapi/src/main/java/com/blog/writeapi/repositories/PostCategoriesRepository.java
@@ -1,8 +1,10 @@
 package com.blog.writeapi.repositories;
 
 import com.blog.writeapi.models.PostCategoriesModel;
+import com.blog.writeapi.utils.annotations.valid.global.isId.IsId;
 import org.jspecify.annotations.NonNull;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PostCategoriesRepository extends JpaRepository<@NonNull PostCategoriesModel, @NonNull Long> {
+    boolean existsByPostIdAndPrimaryTrue(@IsId Long value);
 }
