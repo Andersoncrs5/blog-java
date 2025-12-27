8495962572fcb2845d992281933ab093cb5fb397
andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-12-27 19:29:15 -0300

    Added validations

--- apis/writeapi/src/main/java/com/blog/writeapi/dtos/postTag/CreatePostTagDTO.java
+++ apis/writeapi/src/main/java/com/blog/writeapi/dtos/postTag/CreatePostTagDTO.java
@@ -1,8 +1,21 @@
 package com.blog.writeapi.dtos.postTag;
 
+import com.blog.writeapi.utils.annotations.valid.global.isId.IsId;
+import com.blog.writeapi.utils.annotations.valid.post.existsPostById.ExistsPostById;
+import com.blog.writeapi.utils.annotations.valid.postTag.uniqueTagInPost.UniqueTagInPost;
+import com.blog.writeapi.utils.annotations.valid.tag.existsTagById.ExistsTagById;
+
+@UniqueTagInPost
 public record CreatePostTagDTO(
+
+        @IsId
+        @ExistsPostById
         Long postId,
+
+        @IsId
+        @ExistsTagById
         Long tagId,
+
         Boolean active,
         Boolean visible
 ) {
