3ec109aef28966db1237992eceeb6a36fd2123fa
andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-12-27 12:39:51 -0300

    Impled PostTagRepository

---
+++ apis/writeapi/src/main/java/com/blog/writeapi/repositories/PostTagRepository.java
@@ -0,0 +1,8 @@
+package com.blog.writeapi.repositories;
+
+import com.blog.writeapi.models.PostTagModel;
+import org.jspecify.annotations.NonNull;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PostTagRepository extends JpaRepository<@NonNull PostTagModel, @NonNull Long> {
+}
