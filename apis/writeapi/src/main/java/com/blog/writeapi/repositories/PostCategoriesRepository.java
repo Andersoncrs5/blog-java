f231a1b5b641cc4432f2cd9fcc88aebdc8222cf7
andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-12-26 22:54:55 -0300

    New method existsByPostAndCategory

--- apis/writeapi/src/main/java/com/blog/writeapi/repositories/PostCategoriesRepository.java
+++ apis/writeapi/src/main/java/com/blog/writeapi/repositories/PostCategoriesRepository.java
@@ -1,10 +1,15 @@
 package com.blog.writeapi.repositories;
 
+import com.blog.writeapi.models.CategoryModel;
 import com.blog.writeapi.models.PostCategoriesModel;
+import com.blog.writeapi.models.PostModel;
 import com.blog.writeapi.utils.annotations.valid.global.isId.IsId;
+import jakarta.validation.constraints.NotNull;
 import org.jspecify.annotations.NonNull;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PostCategoriesRepository extends JpaRepository<@NonNull PostCategoriesModel, @NonNull Long> {
     boolean existsByPostIdAndPrimaryTrue(@IsId Long value);
+
+    Boolean existsByPostAndCategory(@NotNull PostModel post, @NotNull CategoryModel category);
 }
