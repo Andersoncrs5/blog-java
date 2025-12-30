da42af4a66f7c7920c5e3216c05cdd62acf56bbc
andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-12-29 21:21:32 -0300

    More validations

--- apis/writeapi/src/main/java/com/blog/writeapi/dtos/comment/UpdateCommentDTO.java
+++ apis/writeapi/src/main/java/com/blog/writeapi/dtos/comment/UpdateCommentDTO.java
@@ -1,22 +1,21 @@
 package com.blog.writeapi.dtos.comment;
 
+import com.blog.writeapi.models.enums.comment.CommentStatusEnum;
 import com.blog.writeapi.utils.annotations.valid.comment.existsCommentById.ExistsCommentById;
 import com.blog.writeapi.utils.annotations.valid.global.isId.IsId;
-import com.blog.writeapi.utils.annotations.valid.post.existsPostById.ExistsPostById;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
-public record CreateCommentDTO(
+public record UpdateCommentDTO(
+        @IsId
+        @ExistsCommentById
+        Long id,
+
 
         @NotBlank
         @Size(min = 1, max = 600)
         String content,
 
-        @IsId
-        @ExistsPostById
-        Long postID,
-
-        @ExistsCommentById
-        Long parentId
+        CommentStatusEnum status
 ) {
 }
