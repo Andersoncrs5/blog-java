a8e502521990246cc1febaba35e5c55d0810dd61
andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-12-29 21:10:03 -0300

    Impled CommentMapper

---
+++ apis/writeapi/src/main/java/com/blog/writeapi/utils/mappers/CommentMapper.java
@@ -0,0 +1,53 @@
+package com.blog.writeapi.utils.mappers;
+
+import com.blog.writeapi.dtos.comment.CommentDTO;
+import com.blog.writeapi.dtos.comment.CreateCommentDTO;
+import com.blog.writeapi.dtos.comment.UpdateCommentDTO;
+import com.blog.writeapi.models.CommentModel;
+import jakarta.validation.constraints.NotNull;
+import org.mapstruct.*;
+
+import java.time.LocalDateTime;
+import java.time.OffsetDateTime;
+import java.time.ZoneOffset;
+
+@Mapper(componentModel = "spring")
+public interface CommentMapper {
+
+    @Mapping(target = "user", source = "author")
+    @Mapping(target = "parentId", source = "parent.id")
+    CommentDTO toDTO(@NotNull CommentModel model);
+
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "post", ignore = true)
+    @Mapping(target = "author", ignore = true)
+    @Mapping(target = "parent", ignore = true)
+    @Mapping(target = "replies", ignore = true)
+    @Mapping(target = "version", ignore = true)
+    @Mapping(target = "createdAt", ignore = true)
+    @Mapping(target = "updatedAt", ignore = true)
+    @Mapping(target = "edited", ignore = true)
+    @Mapping(target = "pinned", ignore = true)
+    @Mapping(target = "ipAddress", ignore = true)
+    CommentModel toModel(@NotNull CreateCommentDTO dto);
+
+    @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "post", ignore = true)
+    @Mapping(target = "author", ignore = true)
+    @Mapping(target = "parent", ignore = true)
+    @Mapping(target = "replies", ignore = true)
+    @Mapping(target = "createdAt", ignore = true)
+    @Mapping(target = "updatedAt", ignore = true)
+    @Mapping(target = "version", ignore = true)
+    @Mapping(target = "ipAddress", ignore = true)
+    void merge(UpdateCommentDTO dto, @MappingTarget CommentModel model);
+
+    default OffsetDateTime map(LocalDateTime dateTime) {
+        return dateTime == null ? null : dateTime.atOffset(ZoneOffset.UTC);
+    }
+
+    default LocalDateTime map(OffsetDateTime offsetDateTime) {
+        return offsetDateTime == null ? null : offsetDateTime.toLocalDateTime();
+    }
+}
