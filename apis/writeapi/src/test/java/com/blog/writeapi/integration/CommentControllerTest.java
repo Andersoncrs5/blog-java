c6df4667fa6438aad9c1dc00f8b4d548c1137dc0
andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-12-31 17:43:07 -0300

    Impled integration test to CommentControllerTest

---
+++ apis/writeapi/src/test/java/com/blog/writeapi/integration/CommentControllerTest.java
@@ -0,0 +1,180 @@
+package com.blog.writeapi.integration;
+
+import com.blog.writeapi.HelperTest;
+import com.blog.writeapi.dtos.comment.CommentDTO;
+import com.blog.writeapi.dtos.comment.CreateCommentDTO;
+import com.blog.writeapi.dtos.post.PostDTO;
+import com.blog.writeapi.repositories.CommentRepository;
+import com.blog.writeapi.repositories.PostCategoriesRepository;
+import com.blog.writeapi.repositories.PostRepository;
+import com.blog.writeapi.utils.res.ResponseHttp;
+import com.blog.writeapi.utils.res.ResponseUserTest;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class CommentControllerTest {
+
+    private final String URL = "/v1/comment";
+
+    @Autowired private MockMvc mockMvc;
+    @Autowired private ObjectMapper objectMapper;
+    @Autowired private HelperTest helper;
+
+    @Autowired private CommentRepository repository;
+    @Autowired private PostCategoriesRepository postCategoriesRepository;
+    @Autowired private PostRepository postRepository;
+
+    @BeforeEach
+    void setup () {
+        this.repository.deleteAll();
+        this.postCategoriesRepository.deleteAll();
+        this.postRepository.deleteAll();
+    }
+
+    // DELETE
+    @Test
+    void shouldDeleteComment() throws Exception {
+        ResponseUserTest userData = this.helper.createUser();
+        PostDTO post = this.helper.createPost(userData);
+        CommentDTO comment = this.helper.createComment(userData, post, null);
+
+        MvcResult result = this.mockMvc.perform(delete(this.URL + "/" + comment.id())
+                        .header("Authorization", "Bearer " + userData.tokens().token()
+                        ))
+                .andExpect(status().isOk())
+                .andReturn();
+
+        String json = result.getResponse().getContentAsString();
+        TypeReference<ResponseHttp<Object>> typeRef = new TypeReference<>() {};
+
+        ResponseHttp<Object> response = objectMapper.readValue(json, typeRef);
+
+        assertThat(response.message()).isNotBlank();
+        assertThat(response.status()).isEqualTo(true);
+
+        assertThat(response.data()).isNull();
+    }
+
+    @Test
+    void shouldReturnForbDeleteComment() throws Exception {
+        ResponseUserTest userData = this.helper.createUser();
+        ResponseUserTest userData2 = this.helper.createUser();
+
+        PostDTO post = this.helper.createPost(userData);
+        CommentDTO comment = this.helper.createComment(userData, post, null);
+
+        this.mockMvc.perform(delete(this.URL + "/" + comment.id())
+                        .header("Authorization", "Bearer " + userData2.tokens().token()
+                        ))
+                .andExpect(status().isForbidden())
+                .andReturn();
+    }
+
+    @Test
+    void shouldReturnNotFoundDeleteComment() throws Exception {
+        ResponseUserTest userData = this.helper.createUser();
+
+        MvcResult result = this.mockMvc.perform(delete(this.URL + "/" + 456374657396476397L)
+                        .header("Authorization", "Bearer " + userData.tokens().token()
+                        ))
+                .andExpect(status().isNotFound())
+                .andReturn();
+
+        String json = result.getResponse().getContentAsString();
+        TypeReference<ResponseHttp<Object>> typeRef = new TypeReference<>() {};
+
+        ResponseHttp<Object> response = objectMapper.readValue(json, typeRef);
+
+        assertThat(response.message()).isNotBlank();
+        assertThat(response.status()).isEqualTo(false);
+
+        assertThat(response.data()).isNull();
+    }
+
+    // CREATE
+    @Test
+    void shouldCreateComment() throws Exception {
+        ResponseUserTest userData = this.helper.createUser();
+        PostDTO post = this.helper.createPost(userData);
+
+        CreateCommentDTO dto = new CreateCommentDTO(
+                "content",
+                post.id(),
+                null
+        );
+
+        MvcResult result = this.mockMvc.perform(post(this.URL)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(dto))
+                        .header("Authorization", "Bearer " + userData.tokens().token()
+                        ))
+                .andExpect(status().isCreated())
+                .andReturn();
+
+        String json = result.getResponse().getContentAsString();
+        TypeReference<ResponseHttp<CommentDTO>> typeRef = new TypeReference<>() {};
+
+        ResponseHttp<CommentDTO> response = objectMapper.readValue(json, typeRef);
+
+        assertThat(response.message()).isNotBlank();
+        assertThat(response.status()).isEqualTo(true);
+
+        assertThat(response.data().id()).isNotNegative().isNotZero();
+        assertThat(response.data().parentId()).isNull();
+        assertThat(response.data().content()).isEqualTo(dto.content());
+        assertThat(response.data().post().id()).isEqualTo(post.id());
+        assertThat(response.data().user().id()).isEqualTo(userData.userDTO().id());
+    }
+
+    @Test
+    void shouldCreateCommentOnComment() throws Exception {
+        ResponseUserTest userData = this.helper.createUser();
+        PostDTO post = this.helper.createPost(userData);
+        CommentDTO comment = this.helper.createComment(userData, post, null);
+
+        CreateCommentDTO dto = new CreateCommentDTO(
+                "content",
+                post.id(),
+                comment.id()
+        );
+
+        MvcResult result = this.mockMvc.perform(post(this.URL)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(dto))
+                        .header("Authorization", "Bearer " + userData.tokens().token()
+                        ))
+                .andExpect(status().isCreated())
+                .andReturn();
+
+        String json = result.getResponse().getContentAsString();
+        TypeReference<ResponseHttp<CommentDTO>> typeRef = new TypeReference<>() {};
+
+        ResponseHttp<CommentDTO> response = objectMapper.readValue(json, typeRef);
+
+        assertThat(response.message()).isNotBlank();
+        assertThat(response.status()).isEqualTo(true);
+
+        assertThat(response.data().id()).isNotNegative().isNotZero();
+        assertThat(response.data().parentId()).isEqualTo(comment.id());
+        assertThat(response.data().content()).isEqualTo(dto.content());
+        assertThat(response.data().post().id()).isEqualTo(post.id());
+        assertThat(response.data().user().id()).isEqualTo(userData.userDTO().id());
+    }
+
+
+}
