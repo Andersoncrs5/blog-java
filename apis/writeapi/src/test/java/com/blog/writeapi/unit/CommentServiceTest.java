f7de8c88a8ce302beeb8be9d53a439bf153db34e
andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-12-31 17:47:45 -0300

    Impled UnitTest to  CommentServiceTest

---
+++ apis/writeapi/src/test/java/com/blog/writeapi/unit/CommentServiceTest.java
@@ -0,0 +1,158 @@
+package com.blog.writeapi.unit;
+
+import cn.hutool.core.lang.Snowflake;
+import com.blog.writeapi.dtos.comment.CreateCommentDTO;
+import com.blog.writeapi.models.CommentModel;
+import com.blog.writeapi.models.PostModel;
+import com.blog.writeapi.models.UserModel;
+import com.blog.writeapi.models.enums.Post.PostStatusEnum;
+import com.blog.writeapi.models.enums.comment.CommentStatusEnum;
+import com.blog.writeapi.repositories.CommentRepository;
+import com.blog.writeapi.services.interfaces.ICommentService;
+import com.blog.writeapi.services.providers.CommentService;
+import com.blog.writeapi.utils.exceptions.ModelNotFoundException;
+import com.blog.writeapi.utils.mappers.CategoryMapper;
+import com.blog.writeapi.utils.mappers.CommentMapper;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+public class CommentServiceTest {
+
+    @Mock private CommentRepository repository;
+    @Mock private Snowflake generator;
+
+    @InjectMocks private CommentService service;
+    @Mock private CommentMapper mapper;
+
+    UserModel user = UserModel.builder()
+            .id(1998780200074176609L)
+            .name("user")
+            .email("user@gmail.com")
+            .password("12345678")
+            .build();
+
+    PostModel post = new PostModel().toBuilder()
+            .id(1998780203274176609L)
+            .title("anyTittle")
+            .slug("any-title")
+            .content("any Content")
+            .status(PostStatusEnum.PUBLISHED)
+            .readingTime(5)
+            .rankingScore(0.0)
+            .isFeatured(false)
+            .author(user)
+            .build();
+
+    CommentModel comment = new CommentModel().toBuilder()
+            .id(1998780200074176609L)
+            .content("content")
+            .status(CommentStatusEnum.APPROVED)
+            .post(this.post)
+            .author(this.user)
+            .parent(null)
+            .edited(true)
+            .pinned(true)
+            .ipAddress("ip-45743567346")
+            .build();
+
+    // METHOD : getById
+    @Test
+    void shouldGetCommentByID() {
+        when(repository.findById(this.comment.getId())).thenReturn(Optional.of(this.comment));
+
+        Optional<CommentModel> optional = this.service.getById(this.comment.getId());
+
+        assertThat(optional.isPresent()).isTrue();
+
+        verifyNoMoreInteractions(repository);
+        verify(repository, times(1)).findById(this.comment.getId());
+    }
+
+    @Test
+    void shouldReturnNullWhenGetCommentByID() {
+        when(repository.findById(this.comment.getId())).thenReturn(Optional.empty());
+
+        Optional<CommentModel> optional = this.service.getById(this.comment.getId());
+
+        assertThat(optional.isEmpty()).isTrue();
+
+        verifyNoMoreInteractions(repository);
+        verify(repository, times(1)).findById(this.comment.getId());
+    }
+
+    // METHOD: delete
+    @Test
+    void shouldDeleteComment() {
+        doNothing().when(repository).delete(this.comment);
+
+        this.service.delete(this.comment);
+
+        verifyNoMoreInteractions(repository);
+        verify(this.repository, times(1)).delete(this.comment);
+    }
+
+    // METHOD: getByIdSimple
+    @Test
+    @DisplayName("Should throw exception when comment not found")
+    void shouldThrowExceptionWhenCommentNotFound() {
+        when(repository.findById(anyLong())).thenReturn(Optional.empty());
+
+        assertThatThrownBy(() -> service.getByIdSimple(1L))
+                .isInstanceOf(ModelNotFoundException.class);
+    }
+
+    @Test
+    void shouldGetCommentByIdWhenExecMethodGetByIdSimple(){
+        when(repository.findById(this.comment.getId())).thenReturn(Optional.of(this.comment));
+
+        CommentModel optional = this.service.getByIdSimple(this.comment.getId());
+
+        verifyNoMoreInteractions(repository);
+        verify(repository, times(1)).findById(this.comment.getId());
+    }
+
+    // METHOD: create
+    @Test
+    @DisplayName("Should create a comment successfully")
+    void shouldCreateAComment() {
+        CreateCommentDTO dto = new CreateCommentDTO(
+                this.comment.getContent(),
+                this.post.getId(),
+                null
+        );
+
+        CommentModel mapped = new CommentModel();
+        mapped.setContent(dto.content());
+
+        when(mapper.toModel(dto)).thenReturn(mapped);
+        when(this.generator.nextId()).thenReturn(this.comment.getId());
+
+        when(repository.save(any(CommentModel.class))).thenAnswer(invocation -> invocation.getArgument(0));
+
+        CommentModel result = this.service.create(dto, this.post, this.user);
+
+        assertThat(result).isNotNull();
+        assertThat(result.getId()).isEqualTo(this.comment.getId());
+        assertThat(result.getAuthor()).isEqualTo(this.user);
+        assertThat(result.getPost()).isEqualTo(this.post);
+        assertThat(result.getContent()).isEqualTo(dto.content());
+
+        verify(mapper, times(1)).toModel(dto);
+        verify(generator, times(1)).nextId();
+        verify(repository, times(1)).save(any(CommentModel.class));
+    }
+
+
+
+}
