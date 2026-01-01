17298a3181ff9bc1505c8b54ba6534b8406ec111
andersoncrs5 <anderson.c.rms2005@gmail.com>
2026-01-01 16:47:41 -0300

    New method findByPostAndUser

--- apis/writeapi/src/main/java/com/blog/writeapi/repositories/PostFavoriteRepository.java
+++ apis/writeapi/src/main/java/com/blog/writeapi/repositories/PostFavoriteRepository.java
@@ -6,6 +6,11 @@ import com.blog.writeapi.models.UserModel;
 import org.jspecify.annotations.NonNull;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface PostFavoriteRepository extends JpaRepository<@NonNull PostFavoriteModel, @NonNull Long > {
     Boolean existsByPostAndUser(PostModel post, UserModel user);
+
+    Optional<PostFavoriteModel> findByPostAndUser(PostModel post, UserModel user);
+
 }
