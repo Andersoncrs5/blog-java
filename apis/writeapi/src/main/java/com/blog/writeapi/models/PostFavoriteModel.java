e57d14e63fc8dfe1f7cc153701606e070cf435eb
andersoncrs5 <anderson.c.rms2005@gmail.com>
2026-01-01 13:27:19 -0300

    New table

---
+++ apis/writeapi/src/main/java/com/blog/writeapi/models/PostFavoriteModel.java
@@ -0,0 +1,45 @@
+package com.blog.writeapi.models;
+
+import com.blog.writeapi.utils.bases.models.BaseEntity;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
+
+@Entity
+@Table(
+        name = "posts_favorites",
+        indexes = {
+                @Index(name = "idx_posts_favorites_post_id", columnList = "post_id"),
+                @Index(name = "idx_posts_favorites_user_id", columnList = "user_id"),
+        },
+        uniqueConstraints = {
+                @UniqueConstraint(name = "uk_posts_favorites", columnNames = {"user_id", "post_id"})
+        }
+)
+@Setter
+@Getter
+@SuperBuilder(toBuilder = true)
+@NoArgsConstructor
+@AllArgsConstructor
+public class PostFavoriteModel extends BaseEntity {
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(
+            name = "post_id",
+            nullable = false,
+            columnDefinition = "BIGINT UNSIGNED"
+    )
+    private PostModel post;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(
+            name = "user_id",
+            nullable = false,
+            columnDefinition = "BIGINT UNSIGNED"
+    )
+    private UserModel user;
+
+}
