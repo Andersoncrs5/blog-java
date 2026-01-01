ffe4f9a98d6f846d0570ffe75cc0b3f3d3c152c7
andersoncrs5 <anderson.c.rms2005@gmail.com>
2026-01-01 13:14:15 -0300

    New endpoint update

--- apis/writeapi/src/main/java/com/blog/writeapi/controllers/docs/CommentControllerDocs.java
+++ apis/writeapi/src/main/java/com/blog/writeapi/controllers/docs/CommentControllerDocs.java
@@ -1,6 +1,7 @@
 package com.blog.writeapi.controllers.docs;
 
 import com.blog.writeapi.dtos.comment.CreateCommentDTO;
+import com.blog.writeapi.dtos.comment.UpdateCommentDTO;
 import com.blog.writeapi.utils.annotations.valid.global.isId.IsId;
 import com.blog.writeapi.utils.res.ResponseHttp;
 import com.blog.writeapi.utils.res.swagger.comment.ResponseCommentDTO;
@@ -15,13 +16,50 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.*;
 
 @Tag(name = "Comment", description = "Endpoints for managing post comments")
 public interface CommentControllerDocs {
+
+    @PatchMapping("/{id}")
+    @CircuitBreaker(name = "tagUpdateCB")
+    @Operation(
+            summary = "Update an existing comment",
+            description = "Updates the content or status of a comment. Only the original author is allowed to perform this action."
+    )
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Comment updated successfully",
+                    content = @Content(schema = @Schema(implementation = ResponseCommentDTO.class))
+            ),
+            @ApiResponse(
+                    responseCode = "400",
+                    description = "Invalid request data or malformed Snowflake ID",
+                    content = @Content(schema = @Schema(implementation = ResponseHttp.class))
+            ),
+            @ApiResponse(
+                    responseCode = "403",
+                    description = "Access denied: You are not the author of this comment",
+                    content = @Content(schema = @Schema(implementation = ResponseHttp.class))
+            ),
+            @ApiResponse(
+                    responseCode = "404",
+                    description = "Comment not found",
+                    content = @Content(schema = @Schema(implementation = ResponseHttp.class))
+            ),
+            @ApiResponse(
+                    responseCode = "503",
+                    description = "Service temporarily unavailable (Circuit Breaker active)",
+                    content = @Content(schema = @Schema(implementation = ResponseHttp.class))
+            )
+    })
+    ResponseEntity<?> update(
+            @Parameter(description = "The unique Snowflake ID of the comment", example = "1998780200074176609")
+            @PathVariable @IsId Long id,
+            @Valid @RequestBody UpdateCommentDTO dto,
+            HttpServletRequest request
+    );
 
     @PostMapping
     @CircuitBreaker(name = "tagCreateCB")
