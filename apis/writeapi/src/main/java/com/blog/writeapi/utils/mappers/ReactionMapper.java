186c1dd3d0ab0caf572a48d37e99ce93dc3a7273
andersoncrs5 <anderson.c.rms2005@gmail.com>
2026-01-07 11:39:35 -0300

    Set up ReactionMapper

---
+++ apis/writeapi/src/main/java/com/blog/writeapi/utils/mappers/ReactionMapper.java
@@ -0,0 +1,40 @@
+package com.blog.writeapi.utils.mappers;
+
+import com.blog.writeapi.dtos.reaction.CreateReactionDTO;
+import com.blog.writeapi.dtos.reaction.ReactionDTO;
+import com.blog.writeapi.dtos.reaction.UpdateReactionDTO;
+import com.blog.writeapi.models.ReactionModel;
+import org.mapstruct.*;
+
+import java.time.LocalDateTime;
+import java.time.OffsetDateTime;
+import java.time.ZoneOffset;
+
+@Mapper(componentModel = "spring")
+public interface ReactionMapper {
+
+    ReactionModel toModel(ReactionDTO dto);
+
+    ReactionDTO toDTO(ReactionModel model);
+
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "version", ignore = true)
+    @Mapping(target = "createdAt", ignore = true)
+    @Mapping(target = "updatedAt", ignore = true)
+    ReactionModel toModel(CreateReactionDTO dto);
+
+    @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "version", ignore = true)
+    @Mapping(target = "createdAt", ignore = true)
+    @Mapping(target = "updatedAt", ignore = true)
+    void merge(UpdateReactionDTO dto, @MappingTarget ReactionModel model);
+
+    default OffsetDateTime map(LocalDateTime dateTime) {
+        return dateTime == null ? null : dateTime.atOffset(ZoneOffset.UTC);
+    }
+
+    default LocalDateTime map(OffsetDateTime offsetDateTime) {
+        return offsetDateTime == null ? null : offsetDateTime.toLocalDateTime();
+    }
+}
