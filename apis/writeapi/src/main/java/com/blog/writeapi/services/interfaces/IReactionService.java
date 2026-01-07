cd7d47a505079081b58c23998f351ec2f6c42f00
andersoncrs5 <anderson.c.rms2005@gmail.com>
2026-01-07 11:38:56 -0300

    Impled ReactionService interface and provider already methods

---
+++ apis/writeapi/src/main/java/com/blog/writeapi/services/interfaces/IReactionService.java
@@ -0,0 +1,16 @@
+package com.blog.writeapi.services.interfaces;
+
+import com.blog.writeapi.dtos.reaction.CreateReactionDTO;
+import com.blog.writeapi.dtos.reaction.UpdateReactionDTO;
+import com.blog.writeapi.models.ReactionModel;
+import com.blog.writeapi.utils.annotations.valid.global.isId.IsId;
+import jakarta.validation.constraints.NotBlank;
+
+public interface IReactionService {
+    ReactionModel getByIdSimple(@IsId Long id);
+    Boolean existsByName(@NotBlank String name);
+    Boolean existsById(@IsId Long id);
+    void delete(ReactionModel reaction);
+    ReactionModel create(CreateReactionDTO dto);
+    ReactionModel update(UpdateReactionDTO dto, ReactionModel reaction);
+}
