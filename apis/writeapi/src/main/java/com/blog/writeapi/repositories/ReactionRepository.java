7043476e2a44e11ab6183528b78c67192c5232a1
andersoncrs5 <anderson.c.rms2005@gmail.com>
2026-01-07 11:39:51 -0300

    Configured ReactionRepository

---
+++ apis/writeapi/src/main/java/com/blog/writeapi/repositories/ReactionRepository.java
@@ -0,0 +1,13 @@
+package com.blog.writeapi.repositories;
+
+import com.blog.writeapi.models.ReactionModel;
+import com.blog.writeapi.utils.annotations.valid.global.isId.IsId;
+import jakarta.validation.constraints.NotBlank;
+import org.jspecify.annotations.NonNull;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ReactionRepository extends JpaRepository<@NonNull ReactionModel, @NonNull Long> {
+    Boolean existsByName(@NotBlank String name);
+}
